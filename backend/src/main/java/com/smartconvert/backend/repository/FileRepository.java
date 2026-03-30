1623c91abe98cc3d3e223650a1c5bbad20fb9100
SaivenkatGajabinkar <you@example.com>
2026-03-30 21:17:49 +0530

    Final Final Version: AI Enhanced and Project Cleaned for Sir!

--- backend/src/main/java/com/smartconvert/backend/repository/FileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartconvert.backend.repository;
-
-import com.smartconvert.backend.model.FileEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileRepository extends CrudRepository<FileEntity, String> {
-}
